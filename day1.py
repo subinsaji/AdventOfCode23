d806407d838c8950369fb653f8c7f78faf0c5fca
Subin Saji <subinssaji@gmail.com>
2023-12-08 16:29:11 +0000

    adding if int logic

---
+++ day1.py
@@ -0,0 +1,18 @@
+# input_text_file = open(r"day1_input.txt")
+# print(input_text_file)
+# input_text_file.read
+
+# doing examples:
+
+example = ["1abc", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+
+string_name =  example[0]
+for element in string_name:
+    if isinstance(element, int) is True:
+        print("pass")
+        
+
+
+
+
+    
