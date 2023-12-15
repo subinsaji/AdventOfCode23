24b01adaf9c87c9f979fcf52128e655820f405e3
Subin Saji <subinssaji@gmail.com>
2023-12-15 16:33:08 +0000

    checks if the code is running for the first time before doing bulk of tasks

--- day1.py
+++ day1.py
@@ -1,28 +1,31 @@
 import numpy as np
 
-# preamble
 filename = "day1_input.txt"
 data = np.loadtxt(filename, dtype=str)
-sum_of_string = []
-sum_of_each_strings = []
+number_in_string = []
 
 for i in range(len(data)):
-    string = data[i]
-    for char in string:
-        if char.isnumeric():
-            sum_of_string.append(int(char))
-            sum_of_each_string = 0
+    first = True
+
+    
+    join_number_in_string = [number_in_string[j] for j in number_in_string]
+
+    if first:
+        string = data[i]
+        for char in string:
+            if char.isnumeric():
+                number_in_string.append(int(char))
 
 
-# for char in string:
-#     if char.isnumeric():
-#         sum_of_string.append(int(char))
-
-
-# number = [str(i) for i in sum_of_string]
+# number = [str(i) for i in number_in_string]
 # p = "".join(number)
 # p = int(p)
 # print(p)
 
-def super_sum():
-    pass
+# def add_numbers():
+#     number = [str(i) for i in sum]
+#     pass
+
+
+# def super_adder():
+#     pass
