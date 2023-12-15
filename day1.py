00c2c80af30235b293e72839581e51e12025e753
Subin Saji <subinssaji@gmail.com>
2023-12-15 15:31:34 +0000

    looping through whole text

--- day1.py
+++ day1.py
@@ -1,19 +1,28 @@
 import numpy as np
 
+# preamble
 filename = "day1_input.txt"
 data = np.loadtxt(filename, dtype=str)
-
-string =  data[1]
 sum_of_string = []
+sum_of_each_strings = []
 
-for char in string:
-    if char.isnumeric():
-        sum_of_string.append(int(char))
-
-number = [str(i) for i in sum_of_string]
-p = "".join(number)
-p = int(p)
-print(p)
-
+for i in range(len(data)):
+    string = data[i]
+    for char in string:
+        if char.isnumeric():
+            sum_of_string.append(int(char))
+            sum_of_each_string = 0
 
 
+# for char in string:
+#     if char.isnumeric():
+#         sum_of_string.append(int(char))
+
+
+# number = [str(i) for i in sum_of_string]
+# p = "".join(number)
+# p = int(p)
+# print(p)
+
+def super_sum():
+    pass
