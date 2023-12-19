26e4e2f6f6eefed4c830ee3bf8c78c65ce00e253
Subin Saji <subinssaji@gmail.com>
2023-12-19 13:03:43 +0000

    joined all, works but only needed to join the first and last

--- day1.py
+++ day1.py
@@ -1,45 +1,17 @@
 import numpy as np
+import re
 
 filename = "day1_input.txt"
-data = np.loadtxt(filename, dtype=str)
-number_in_string = []
-first = True
+string_list = np.loadtxt(filename, dtype=str)
 
-for i in range(len(data)):
-    join_number_in_string = [number_in_string[j] for j in number_in_string]
-    p = "".join(join_number_in_string)
-    #p = int(p)
+def sum_of_joined_digits(string_list):
+    numbers  = []
+    for s in string_list:
+        number = "".join(re.findall(r"\d", s))
+        if number:
+            numbers.append(int(number))
 
-    sum_of_string = sum(p)
-    string = data[i]
-    for char in string:
-        if char.isnumeric():
-            number_in_string.append(int(char))
-
-    
+    return sum(numbers)
 
 
-
-# def joining_function(join_number_string):
-#     join_number_in_string = [number_in_string[j] for j in number_in_string]
-#     p = "".join(join_number_in_string)
-#     p = int(p)
-#     p = np.array(p)
-#     print(p)
-#     return p
-
-
-
-
-# number = [str(i) for i in number_in_string]
-# p = "".join(number)
-# p = int(p)
-# print(p)
-
-# def add_numbers():
-#     number = [str(i) for i in sum]
-#     pass
-
-
-# def super_adder():
-#     pass
+print(sum_of_joined_digits(string_list))
